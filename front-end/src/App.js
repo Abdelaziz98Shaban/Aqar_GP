2a15f8c697974908dd4a38606a6bf325656bd681
asema <asema@LAPTOP-B1IQUE15>
2022-03-04 14:51:15 +0200

    Add: ProductCardUi

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,13 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import React from "react";
 import NavBar from "./components/NavBar/NavBar";
 import Home from "./pages/Home";
-
+import Property from "./components/Property";
 function App() {
   return (
     <>
       <NavBar />
       <Routes>
         <Route exact path='/' element={<Home />} />
+        <Route path='/property' element={<Property/>}/>
       </Routes>
     </>
   );
