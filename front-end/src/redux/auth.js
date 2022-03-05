fd4261650194bcdd6760232d89de18f07ac77282
elsayed136 <Elsayed_Ahmed_Eltaweel@outlook.com>
2022-03-05 21:43:25 +0200

    Add: log out auth

--- front-end/src/redux/auth.js
+++ front-end/src/redux/auth.js
@@ -2,7 +2,14 @@ import { createSlice } from "@reduxjs/toolkit";
 import { apiCallBegan } from "./api";
 
 const initialState = {
-  currentUser: null,
+  isAuthenticated: false,
+  username: "",
+  email: "",
+  roles: [],
+  token: "",
+  expiresOn: "",
+  refreshTokenExpiration: "",
+  loading: false,
   error: null,
 };
 const authSlice = createSlice({
@@ -15,7 +22,13 @@ const authSlice = createSlice({
       auth.error = null;
     },
     loginReceived: (auth, action) => {
-      auth.currentUser = action.payload;
+      auth.isAuthenticated = action.payload.isAuthenticated;
+      auth.username = action.payload.username;
+      auth.email = action.payload.email;
+      auth.roles = action.payload.roles;
+      auth.token = action.payload.token;
+      auth.expiresOn = action.payload.expiresOn;
+      auth.refreshTokenExpiration = action.payload.refreshTokenExpiration;
       auth.loading = false;
       auth.error = null;
     },
@@ -23,11 +36,31 @@ const authSlice = createSlice({
       auth.loading = false;
       auth.error = action.payload;
     },
+    logoutRequest: auth => {
+      auth.isAuthenticated = null;
+      auth.username = null;
+      auth.email = null;
+      auth.roles = null;
+      auth.token = null;
+      auth.expiresOn = null;
+      auth.refreshTokenExpiration = null;
+      auth.loading = false;
+      auth.error = null;
+    },
+    logoutRequestFailed: (auth, action) => {
+      auth.loading = false;
+      auth.error = action.payload;
+    },
   },
 });
 
-export const { loginRquest, loginReceived, loginRequestedFailed } =
-  authSlice.actions;
+export const {
+  loginRquest,
+  loginReceived,
+  loginRequestedFailed,
+  logoutRequest,
+  logoutRequestFailed,
+} = authSlice.actions;
 
 export default authSlice.reducer;
 
@@ -46,15 +79,14 @@ export const login = data => dispatch => {
   );
 };
 
-// export const getSearchValue = data => dispatch => {
-//   return dispatch(
-//     apiCallBegan({
-//       url: "/Search",
-//       method: "POST",
-//       data,
-//       onStart: searchRquest.type,
-//       onSuccess: searchReceived.type,
-//       onError: searchRequestedFailed.type,
-//     })
-//   );
-// };
+export const logout = data => dispatch => {
+  return dispatch(
+    apiCallBegan({
+      url: "/Auth/logout",
+      method: "POST",
+      data,
+      onSuccess: logoutRequest.type,
+      onError: logoutRequestFailed.type,
+    })
+  );
+};
